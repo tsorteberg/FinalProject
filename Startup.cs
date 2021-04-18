883d953c4c234d6d7985f5da40ffed5fd95d1bb9
Tom Sorteberg <66091315+tsorteberg@users.noreply.github.com>
2021-04-18 10:49:19 -0500

    Added requisite packages via NuGet and added files: SessionExtensionsMethods.cs, StringExtensionsMethods.cs.  Updated documentation.

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,17 @@
+/***************************************************************
+* Name        : Startup.cs
+* Author      : Tom Sorteberg
+* Created     : 04/18/2021
+* Course      : CIS 174
+* Version     : 1.0
+* OS          : Windows 10 Pro, Visual Studio Community 2019
+* Copyright   : This is my own original work based on
+*               specifications issued by our instructor
+* Description : Final Project 
+* I have not used unauthorized source code, either modified or
+* unmodified. I have not given other fellow student(s) access
+* to my program.
+***************************************************************/
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -22,7 +36,9 @@ namespace FinalProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddMemoryCache();
+            services.AddSession();
+            services.AddControllersWithViews().AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +57,8 @@ namespace FinalProject
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
