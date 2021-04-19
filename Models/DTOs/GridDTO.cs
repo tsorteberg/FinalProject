0d4d703f359fffb1d8c1a64db97be6418beec0bf
Tom Sorteberg <66091315+tsorteberg@users.noreply.github.com>
2021-04-18 23:40:10 -0500

    Added GridDTO.cs.

---
+++ Models/DTOs/GridDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    // General purpose class for model binding the paging and sorting route segments
+    // defined in the Startup.cs file - can be used for any web application. Doesn't
+    // include any filter properties, as those are usually application-specific. 
+
+    public class GridDTO
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 4;
+        public string SortField { get; set; }
+        public string SortDirection { get; set; } = "asc";
+    }
+}
